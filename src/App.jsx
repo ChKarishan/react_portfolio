de96df5e756790d8cc89dc4a646499deaaf0f5b5
karishan <karishan.897@gmail.com>
2023-07-20 11:05:22 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+import './index.css'
+import Header from './components/header/header'
+import Nav from './components/nav/nav'
+import About from './components/about/About'
+import Experience from './components/experience/Experience'
+import Services from './components/services/services'
+import Portfolio from './components/portfolio/portfolio'
+import Testimonals from './components/testimonals/testimonals'
+import Contact from './components/Contact/Contact'
+import Footer from './components/footer/footer'
+
+const App = () => {
+  return (
+    <>
+    <Header />
+    <Nav />
+    <About />
+    <Experience />
+    <Services />
+    <Portfolio />
+    <Testimonals />
+    <Contact />
+    <Footer />
+    </>
+  )
+}
+
+export default App
