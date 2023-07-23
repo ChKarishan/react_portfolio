11fc7265424c68bf6f8efef55d68877a0fc4e2d6
karishan <karishan.897@gmail.com>
2023-07-24 02:49:47 +0500

    Footer Done (next: scroll_down, services_lists)

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,9 +1,29 @@
 import React from 'react'
 import './footer.css'
+import {FaFacebook} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
 
 const footer = () => {
   return (
-    <div>footer</div>
+    <footer>
+      <a href='#' className='footer__logo'>Karishan Mehmood</a>
+
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+
+      <div className='footer__socials'>
+        <a href='https://facebook.com' target='_blank'><FaFacebook/></a>
+        <a href='https://instagram.com' target='_blank'><FiInstagram/></a>
+        <a href='https://twitter.com' target='_blank'><IoLogoTwitter/></a>
+      </div>
+
+    </footer>
   )
 }
 
