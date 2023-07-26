3c2357ae1896901c92bab01a1bde710e35be2a6d
karishan <karishan.897@gmail.com>
2023-07-26 14:44:50 +0500

    whatsapp done , links done

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -6,9 +6,9 @@ import {FaTwitter} from 'react-icons/fa'
 const HeaderSocials = () => {
   return (
     <div className='header__socials'>
-        <a href="https://linkedin.com" target = "_blank"><BsLinkedin/></a>
-        <a href="https://github.com" target = "_blank"><FaGithub/></a>
-        <a href="https://twitter.com" target = "_blank"><FaTwitter/></a>
+        <a href="https://www.linkedin.com/in/karishan-mehmood-8a8b1b1a1/" target = "_blank"><BsLinkedin/></a>
+        <a href="https://github.com/ChKarishan" target = "_blank"><FaGithub/></a>
+        <a href="https://twitter.com/KarishanMehmood" target = "_blank"><FaTwitter/></a>
         </div>
   )
 }
